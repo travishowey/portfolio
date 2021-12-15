be3e904296baa8b579e660cef104ec0f98ddcf8b
travishowey <77299383+travishowey@users.noreply.github.com>
2021-12-15 16:22:17 -0700

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,19 @@
+$('.brand-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:false,
+    responsive:{
+      0:{
+        items:2,
+        autoplay: true
+      },
+      600:{
+        items:4,
+        autoplay: true
+      },
+      1000:{
+        items:5
+      }
+    }
+  }) 
+  
